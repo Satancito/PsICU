5d0efda2bae8035daeeb99dda91e0a1200ea1882
Joma <megamanx@outlook.com>
2024-04-28 00:53:06 -0500

    Android script completed. ICU version updated.

--- X-PsIcu-Android.ps1
+++ X-PsIcu-Android.ps1
@@ -2,20 +2,138 @@
 param (
     [Parameter()]
     [string]
-    $AndroidAPI,
+    $AndroidAPI = [string]::Empty,
 
     [string]
     $DestinationDir = [string]::Empty,
+
+    [string]
+    $DistDirSuffix = [string]::Empty,
 
     [switch]
     $ForceDownloadNDK,
 
     [switch]
-    $ForceDownloadICU
+    $ForceDownloadICU,
+
+    [string[]]
+    $IcuConfigureParameters = @() 
 )
 
 Import-Module -Name "$PSScriptRoot/Z-PsIcu.ps1" -Force -NoClobber
-if([string]::IsNullOrWhiteSpace($AndroidAPI))
-{
-    $AndroidAPI = [AndroidND]
+
+function Test-WindowsDependencyTools {
+    Write-Host
+    Write-InfoBlue "PsIcu - Android - Test Windows dependency tools..."
+    Assert-7ZipExecutable
+    Assert-WslExecutable
+}
+
+function Test-DependencyTools {
+    Write-InfoBlue "PsIcu - Android - Test dependency tools..."
+    Write-Host
+    Assert-MakeExecutable
+    Assert-UnzipExecutable
+}
+
+if ([string]::IsNullOrWhiteSpace($AndroidAPI)) {
+    $AndroidAPI = [AndroidNDKApiValidateSet]::ValidValues | Select-Object -Last 1
+}
+
+Assert-AndroidNDKApi -Api $AndroidAPI
+
+$DestinationDir = [string]::IsNullOrWhiteSpace($DestinationDir) ? "$(Get-CppLibsDir)" : $DestinationDir
+
+if ($IsWindows) {
+    Test-WindowsDependencyTools
+    $scriptParameters = @{
+        Script           = (Get-WslPath -Path "$PSCommandPath")
+        AndroidAPI       = $AndroidAPI
+        DestinationDir   = (Get-WslPath -Path "$DestinationDir")
+        DistDirSuffix    = $DistDirSuffix
+        ForceDownloadNDK = $ForceDownloadNDK.IsPresent
+        ForceDownloadICU = $ForceDownloadICU.IsPresent
+    }
+    Write-Warning "Incompatible platform: Windows. Using WSL."
+    & wsl pwsh -Command {
+        $params = $args[0]
+        Write-Host "Wsl User: " -NoNewline ; & whoami
+        & "$($params.Script)" `
+            -AndroidAPI $params.AndroidAPI `
+            -DestinationDir $params.DestinationDir `
+            -DistDirSuffix $params.DistDirSuffix `
+            -ForceDownloadNDK:$params.ForceDownloadNDK `
+            -ForceDownloadICU:$params.ForceDownloadICU
+
+    } -args $scriptParameters
+    return
+}
+
+$DistDirSuffix = [string]::IsNullOrWhiteSpace($DistDirSuffix) ? [string]::Empty : "-$($DistDirSuffix)"
+
+Test-DependencyTools
+Install-AndroidNDK -Force:$ForceDownloadNDK
+Get-IcuSources -Force:$ForceDownloadICU
+
+$preprocessorFlags = "-ffunction-sections -fdata-sections -fvisibility=hidden -fno-short-wchar -fno-short-enums -DU_USING_ICU_NAMESPACE=1 -DU_HAVE_NL_LANGINFO_CODESET=0 -D__STDC_INT64__ -DU_TIMEZONE=0 -DUCONFIG_NO_LEGACY_CONVERSION=1 -DU_CHARSET_IS_UTF8=1"
+$ldFlags = "$(Get-IcuAndroidHostLdFlags)"
+$__PSICU_HOST_BUILD_CONFIGURATIONS.Keys | ForEach-Object {
+    $configuration = $__PSICU_HOST_BUILD_CONFIGURATIONS["$_"]
+    try {
+        Write-Host
+        Write-InfoBlue "█ PsIcu - Building host lib - Configuration: $($configuration.Name) - Host Platform: $(Get-IcuAndroidHostPlatform)"
+        Write-Host
+        $env:CPPFLAGS = "$preprocessorFlags"
+        $env:LDFLAGS = "$ldFlags"
+        New-Item -Path "$($configuration.CurrentWorkingDir)" -ItemType Directory -Force | Out-Null
+        Push-Location "$($configuration.CurrentWorkingDir)"
+        $prefix = "$($configuration.CurrentWorkingDir)/Dist"
+        $null = Test-ExternalCommand -Command "sh `"$__PSICU_ICU_SOURCE_DIR/runConfigureICU`" $($configuration.ConfigurationOption) `"$(Get-IcuAndroidHostPlatform)`" --prefix=`"$prefix`" $($configuration.IcuConfigureParameters) $IcuConfigureParameters" -ThrowOnFailure -NoAssertion
+        $null = Test-ExternalCommand -Command "make -j16" -ThrowOnFailure -NoAssertion
+        Remove-Item -Path "$prefix" -Force -Recurse -ErrorAction Ignore
+        $null = Test-ExternalCommand -Command "make install" -ThrowOnFailure -NoAssertion
+    }
+    finally {
+        Pop-Location
+    } 
+}
+
+$ndkProps = $__PSCOREFXS_ANDROID_NDK_OS_VARIANTS["$(Get-OsName -Minimal)"]
+
+$ldFlags = "-lc -lstdc++ -Wl,--gc-sections"
+$__PSICU_ANDROID_BUILD_CONFIGURATIONS.Keys | ForEach-Object {
+    $configuration = $__PSICU_ANDROID_BUILD_CONFIGURATIONS["$_"]
+    $hostConfiguration = $__PSICU_HOST_BUILD_CONFIGURATIONS["$($configuration.Name)"]
+    $target = "$($configuration.Triplet)"
+    $toolchainsDir = "$($ndkProps.ToolchainsDir)"
+
+    $env:TARGET = $target
+    $env:API = $AndroidAPI
+    $env:AR = "$toolchainsDir/bin/llvm-ar"
+    $env:CC = "$toolchainsDir/bin/$target$AndroidAPI-clang"
+    $env:AS = $CC 
+    $env:CXX = "$toolchainsDir/bin/$target$AndroidAPI-clang++"
+    $env:LD = "$toolchainsDir/bin/ld"
+    $env:RANLIB = "$toolchainsDir/bin/llvm-ranlib"
+    $env:STRIP = "$toolchainsDir/bin/llvm-strip"
+    $env:CPPFLAGS = $preprocessorFlags
+    $env:LDFLAGS = $ldFlags
+    $env:CFLAGS = [string]::Empty
+    $env:CXXFLAGS = [string]::Empty
+
+    try {
+        $prefix = "$DestinationDir/$([string]::Format($configuration.DistDirName, $AndroidAPI))$DistDirSuffix"
+        Write-Host
+        Write-InfoBlue "█ PsIcu - Building `"$prefix`""
+        Write-Host
+        New-Item -Path "$($configuration.CurrentWorkingDir)" -ItemType Directory -Force | Out-Null
+        Push-Location "$($configuration.CurrentWorkingDir)"
+        $null = Test-ExternalCommand -Command "sh `"$__PSICU_ICU_SOURCE_DIR/configure`" --host=`"$target`" --with-cross-build=`"$($hostConfiguration.CurrentWorkingDir)`" $($configuration.ConfigurationOption) --prefix=`"$prefix`" $($configuration.Options) $IcuConfigureParameters " -ThrowOnFailure -NoAssertion
+        $null = Test-ExternalCommand -Command "make -j16" -ThrowOnFailure -NoAssertion
+        Remove-Item -Path "$prefix" -Force -Recurse -ErrorAction Ignore
+        $null = Test-ExternalCommand -Command "make install" -ThrowOnFailure -NoAssertion
+    }
+    finally {
+        Pop-Location
+    }
 }
---
+++ Z-PsIcu.ps1
@@ -0,0 +1,149 @@
+Import-Module -Name "$PSScriptRoot/submodules/PsCoreFxs/Z-PsCoreFxs.ps1" -Force -NoClobber
+
+# MARK: █ Constants
+$__PSICU_GITHUB_URL = "https://github.com/Satancito/PsICU.git"; $null = $__PSICU_GITHUB_URL
+$__PSICU_TEMP_DIR = "$(Get-UserHome)/.PsIcu"; $null = $__PSICU_TEMP_DIR
+$__PSICU_ICU_VERSION = "75.1" #Update on next version
+$__PSICU_ICU_DOWNLOAD_SHA1 = "46619717D43E9C0D028C89EE5D9AE9263C87256E"; # Update on next version
+$__PSICU_ICU_DOWNLOAD_URL = "https://github.com/unicode-org/icu/archive/refs/tags/release-$($__PSICU_ICU_VERSION.Replace(".", "-")).zip"; $null = $__PSICU_ICU_DOWNLOAD_URL
+$__PSICU_ICU_DOWNLOAD_NAME = "icu-release-$($__PSICU_ICU_VERSION.Replace(".", "-")).zip"
+$__PSICU_ICU_DOWNLOAD_FILENAME = "$__PSICU_TEMP_DIR/$__PSICU_ICU_DOWNLOAD_NAME"; $null = $__PSICU_ICU_DOWNLOAD_FILENAME
+$__PSICU_ICU_SOURCE_DIR = "$__PSICU_TEMP_DIR/$([System.IO.Path]::GetFileNameWithoutExtension($__PSICU_ICU_DOWNLOAD_NAME))/icu4c/source"; $null = $__PSICU_ICU_SOURCE_DIR
+$__PSICU_ICU_ROOT_DIR = "$__PSICU_TEMP_DIR/$([System.IO.Path]::GetFileNameWithoutExtension($__PSICU_ICU_DOWNLOAD_NAME))"; $null = $__PSICU_ICU_ROOT_DIR
+
+function Get-IcuAndroidHostLdFlags {
+    if ($IsLinux) {
+        return "-Wl,--gc-sections"
+    }
+    if ($IsMacOS) {
+        return "-Wl,-dead_strip"
+    }
+}
+
+$__PSICU_HOST_BUILD_CONFIGURATIONS = [ordered]@{
+    Debug   = [ordered]@{
+        Name                   = "Debug"
+        CurrentWorkingDir      = "$__PSICU_ICU_SOURCE_DIR/Bin/Debug/Host"
+        ConfigurationOption         = @("--enable-debug")
+        IcuConfigureParameters = @("--enable-static=yes", "--enable-shared=yes")
+    }
+    Release = [ordered]@{
+        Name                   = "Release"
+        CurrentWorkingDir      = "$__PSICU_ICU_SOURCE_DIR/Bin/Release/Host"
+        ConfigurationOption         = @()
+        IcuConfigureParameters = @("--enable-static=yes", "--enable-shared=yes")
+
+    }
+}; $null = $__PSICU_HOST_BUILD_CONFIGURATIONS
+
+# MARK: █ Android build configurations
+$__PSICU_ANDROID_BUILD_CONFIGURATIONS = [ordered]@{
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)-Debug"     = @{ 
+        Name              = "Debug"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Triplet)"
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @("--enable-debug")
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Debug/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)-Debug"     
+    }
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)-Debug"   = @{ 
+        Name              = "Debug"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Triplet)"
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @("--enable-debug")
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Debug/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)-Debug"     
+    }
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)-Debug"     = @{ 
+        Name              = "Debug"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Triplet)"
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @("--enable-debug")
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Debug/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)-Debug"   
+    }
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)-Debug"     = @{ 
+        Name              = "Debug"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Triplet)"
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @("--enable-debug")
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Debug/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)-Debug"   
+    }
+
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)-Release"   = @{ 
+        Name              = "Release"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Triplet)"
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @()
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Release/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm.Name)-Release"
+    }
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)-Release" = @{ 
+        Name              = "Release"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Triplet)" 
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @()
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Release/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.Arm64.Name)-Release" 
+    }
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)-Release"   = @{ 
+        Name              = "Release"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Triplet)"
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @()
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Release/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X86.Name)-Release" 
+    }
+    "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)-Release"   = @{ 
+        Name              = "Release"
+        AbiName           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)"
+        Abi               = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Abi)"
+        Triplet           = "$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Triplet)"
+        Options           = @("--enable-static=yes", "--enable-shared=yes")
+        ConfigurationOption    = @()
+        CurrentWorkingDir = "$__PSICU_ICU_SOURCE_DIR/Bin/Release/Android-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)"
+        DistDirName       = "ICU-$__PSICU_ICU_VERSION-Android-Api{0}-$($__PSCOREFXS_ANDROIDNDK_ANDROID_ABI_CONFIGURATIONS.X64.Name)-Release"  
+    }
+}; $null = $__PSICU_ANDROID_BUILD_CONFIGURATIONS
+
+# MARK: █ Functions
+
+function Get-IcuAndroidHostPlatform {
+    if ($IsLinux) {
+        return "Linux"
+    }
+    if ($IsMacOS) {
+        return "MacOSX/GCC"
+    }
+}
+function Get-IcuSources {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [switch]
+        $Force
+    )
+    Invoke-HttpDownload -Url "$__PSICU_ICU_DOWNLOAD_URL" -DestinationPath "$__PSICU_TEMP_DIR"  -Name "$__PSICU_ICU_DOWNLOAD_NAME" -Force:$Force -HashAlgorithm "SHA1" -Hash "$__PSICU_ICU_DOWNLOAD_SHA1"
+    $rootDirExists = $(Test-Path -Path "$($__PSICU_ICU_ROOT_DIR)" -PathType Container)
+    if (!$rootDirExists -or $Force.IsPresent) {
+        Expand-ZipArchive -Path "$($__PSICU_ICU_DOWNLOAD_FILENAME)" -DestinationPath "$__PSICU_TEMP_DIR"
+    }
+}
+
+
+
